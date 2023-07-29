e5ecf4a11f02cd107c7f946dce45e84d50263a77
coder123-project <83872425+coder123-project@users.noreply.github.com>
2023-07-28 18:52:12 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDnI0GSUYnvg7fmYZDSWBUtwt3e0Nb-Azk",
+  authDomain: "storytellin-effb2.firebaseapp.com",
+  databaseURL: "https://storytellin-effb2-default-rtdb.firebaseio.com",
+  projectId: "storytellin-effb2",
+  storageBucket: "storytellin-effb2.appspot.com",
+  messagingSenderId: "971127566983",
+  appId: "1:971127566983:web:11aa8be8091a54e6623e22",
+  measurementId: "G-MVCRSZPZLW"
+};
